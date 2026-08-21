e78ad100c99b5832a44732f523052c2302a79053
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-08-21 23:59:41 +0100

    Update and rename bash to bash.zsh

---
+++ bash.zsh
@@ -0,0 +1,8 @@
+npm install -g @github/copilot
+/var/containers/Bundle/Application/ED889B78-A0A0-4C16-9220-62509C4A0AB7/stable.app/Chrome \
+  --gaia-url=https://accounts.google.com/ \
+  --lso-url=https://groups.google.com/g/qubuhub \
+  --google-apis-url=https://oauth2.googleapis.com/ \
+  --flag-switches-begin \
+  --enable-features=GeminiBackendMigration,GeminiBinaryMigration,GeminiChatPersistence,GeminiLive,GeminiLuminous,GeminiScreenContextMigration,GeminiUnaryMigration,GeminiUpdatedEligibility,PageActionMenuAuthFlow,ZeroStateSuggestionsWCGD \
+  --flag-switches-end
